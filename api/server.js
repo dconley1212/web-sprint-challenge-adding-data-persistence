bdfdf86eff7ecc06694c1aeef1c4ace1b55643d3
David Conley <dconley1989@gmail.com>
2022-02-18 08:42:10 -0700

    set up the structure of the project

--- api/server.js
+++ api/server.js
@@ -1,1 +1,13 @@
 // build your server here and require it from index.js
+const express = require("express");
+const morgan = require("morgan");
+const server = express();
+
+server.use(express.json());
+server.use(morgan("tiny"));
+
+server.get("/", (res, req) => {
+  res.send(`<h1>Welcome to the sprint challenge Server</h1>`);
+});
+
+module.exports = server;
